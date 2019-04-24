209ef7f49bc538e059bc6573587b67b6d62acb9b
Zalkin <Zalkin.viktor@mail.ru>
2019-04-24 20:58:28 +0300

    M.TFH. Implement Read()

--- Crypter/Model/TxtFileHandler.cs
+++ Crypter/Model/TxtFileHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Crypter.Model
 {
@@ -10,7 +11,7 @@ namespace Crypter.Model
     {
         public string Read(string filename)
         {
-            throw new NotImplementedException();
+            return File.ReadAllText(filename);
         }
 
         public void Save(string filename, string text)
