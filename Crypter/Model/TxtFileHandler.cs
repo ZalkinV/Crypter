fa2acbdd02a9192b3f8f98fb257f1f9ff47e5c47
Zalkin <Zalkin.viktor@mail.ru>
2019-04-24 20:56:13 +0300

    Model. Create class TxtFileHandler(TFH)

---
+++ Crypter/Model/TxtFileHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypter.Model
+{
+    class TxtFileHandler : IFileHandler
+    {
+        public string Read(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string filename, string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
