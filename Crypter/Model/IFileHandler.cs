0200b9dcee8c82f869f31a27631deea127cde324
Zalkin <Zalkin.viktor@mail.ru>
2019-04-24 20:53:05 +0300

    Model. Create interface IFileHandler(IFH)

---
+++ Crypter/Model/IFileHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypter.Model
+{
+    interface IFileHandler
+    {
+        string Read(string filename);
+        void Save(string filename, string text);
+    }
+}
