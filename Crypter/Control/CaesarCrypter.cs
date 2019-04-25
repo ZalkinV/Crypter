17da827a0eca6baecd1057127d9527c5f2593090
Zalkin <Zalkin.viktor@mail.ru>
2019-04-25 12:08:06 +0300

    C.CC. Add implementation to Encrypt()

--- Crypter/Control/CaesarCrypter.cs
+++ Crypter/Control/CaesarCrypter.cs
@@ -10,7 +10,21 @@ namespace Crypter.Control
     {
         public static string Encrypt(string text, int step)
         {
-            return text;
+            char[] symbols = text.ToCharArray();
+
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                if (char.IsDigit(symbols[i]))
+                {
+                    symbols[i] = DoShift(symbols[i], step, '0', '9');
+                }
+                if ('а' <= char.ToLower(symbols[i]) && char.ToLower(symbols[i]) <= 'я')
+                {
+                    symbols[i] = DoShift(symbols[i], step, 'а', 'я');
+                }
+            }
+
+            return new string(symbols);
         }
 
         public static string Decrypt(string text, int step)
