e6d3f7e85ee70ea3e3390cfd71354a44acfcb157
Zalkin <Zalkin.viktor@mail.ru>
2019-04-26 17:01:06 +0300

    M.DFH.Read(). Add implementation

--- Crypter/Model/DocxFileHandler.cs
+++ Crypter/Model/DocxFileHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xceed.Words.NET;
 
 namespace Crypter.Model
 {
@@ -10,7 +11,10 @@ namespace Crypter.Model
     {
         public string Read(string filename)
         {
-            throw new NotImplementedException();
+            using (DocX document = DocX.Load(filename))
+            {
+                return document.Text;
+            }
         }
 
         public void Save(string filename, string text)
