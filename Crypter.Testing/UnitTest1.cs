4c44da5af6288a1d0dce42a387e6b515e4ff5e3e
Zalkin <Zalkin.viktor@mail.ru>
2019-04-27 09:36:02 +0300

    Create testing project Crypter.Testing(CT)

---
+++ Crypter.Testing/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Crypter.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
