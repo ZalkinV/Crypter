5a40df8b343ae3f6901c4e343ac883d7362d77a2
Zalkin <Zalkin.viktor@mail.ru>
2019-04-27 22:03:29 +0300

    CT.RT. Implement TestDifferentShift()

--- Crypter.Testing/RussianTest.cs
+++ Crypter.Testing/RussianTest.cs
@@ -23,5 +23,23 @@ namespace Crypter.Testing
             Assert.AreEqual(expectedEncrypt, actualEncrypt);
             Assert.AreEqual(expectedDecrypt, actualDecrypt);
         }
+
+        [TestMethod]
+        public void TestDifferentShift()
+        {
+            string text = "ая";
+            int[] steps = new int[] { 0, 1, 32, 33, 34, 66, 67 };
+            string[] expectedEncrypts = new string[] { "ая", "ба", "яю", "ая", "ба", "ая", "ба" };
+            string[] expectedDecrypts = new string[] { "ая", "яю", "ба", "ая", "яю", "ая", "яю" };
+
+            for (int i = 0; i < steps.Length; i++)
+            {
+                string actualEncrypt = crypterRus.Encrypt(text, steps[i]);
+                string actualDecrypt = crypterRus.Decrypt(text, steps[i]);
+
+                Assert.AreEqual(expectedEncrypts[i], actualEncrypt);
+                Assert.AreEqual(expectedDecrypts[i], actualDecrypt);
+            }
+        }
     }
 }
