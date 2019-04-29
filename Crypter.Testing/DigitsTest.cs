71eebab1007451c5bc434e5274644e0ed3fa774b
Zalkin <Zalkin.viktor@mail.ru>
2019-04-29 20:47:58 +0300

    CT.DT. Implement TestDifferentShift()

--- Crypter.Testing/DigitsTest.cs
+++ Crypter.Testing/DigitsTest.cs
@@ -18,5 +18,20 @@ namespace Crypter.Testing
             CommonTestMethods.TestShift(crypterDig.Encrypt, text, step, "234");
             CommonTestMethods.TestShift(crypterDig.Decrypt, text, step, "012");
         }
+
+        [TestMethod]
+        public void TestDifferentShift()
+        {
+            string text = "09";
+            int[] steps = new int[] { 0, 1, 9, 10, 11, 20, 21 };
+            string[] expectedEncrypts = new string[] { "09", "10", "98", "09", "10", "09", "10" };
+            string[] expectedDecrypts = new string[] { "09", "98", "10", "09", "98", "09", "98" };
+
+            for (int i = 0; i < steps.Length; i++)
+            {
+                CommonTestMethods.TestShift(crypterDig.Encrypt, text, steps[i], expectedEncrypts[i]);
+                CommonTestMethods.TestShift(crypterDig.Decrypt, text, steps[i], expectedDecrypts[i]);
+            }
+        }
     }
 }
