460634a7bd7e03a7bc66aa0e43cef4f8246f1256
Zalkin <Zalkin.viktor@mail.ru>
2019-04-29 23:23:06 +0300

    CT.MLT. Implement TestRusDig()

--- Crypter.Testing/MixedLanguagesTest.cs
+++ Crypter.Testing/MixedLanguagesTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Crypter.Control;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Crypter.Testing
@@ -7,5 +8,16 @@ namespace Crypter.Testing
     public class MixedLanguagesTest
     {
         private const string punctuation = ".,?!-#'\";:()";
+
+        [TestMethod]
+        public void TestRusDig()
+        {
+            CaesarCrypter crypter = new CaesarCrypter(new Alphabet(Languages.Russian), new Alphabet(Languages.Digits));
+            string text = "бвг123abc" + punctuation;
+            int step = 1;
+
+            CommonTestMethods.TestShift(crypter.Encrypt, text, step, "вгд234abc" + punctuation);
+            CommonTestMethods.TestShift(crypter.Decrypt, text, step, "абв012abc" + punctuation);
+        }
     }
 }
