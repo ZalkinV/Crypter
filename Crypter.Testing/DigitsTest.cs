8dad61feba73d24653fd6a32036260e567e8f598
Zalkin <Zalkin.viktor@mail.ru>
2019-04-29 20:24:07 +0300

    CT.DT. Implement TestSimpleShift()

--- Crypter.Testing/DigitsTest.cs
+++ Crypter.Testing/DigitsTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Crypter.Control;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Crypter.Testing
@@ -6,5 +7,16 @@ namespace Crypter.Testing
     [TestClass]
     public class DigitsTest
     {
+        CaesarCrypter crypterDig = new CaesarCrypter(new Alphabet(Languages.Digits));
+
+        [TestMethod]
+        public void TestSimpleShift()
+        {
+            string text = "123";
+            int step = 1;
+
+            CommonTestMethods.TestShift(crypterDig.Encrypt, text, step, "234");
+            CommonTestMethods.TestShift(crypterDig.Decrypt, text, step, "012");
+        }
     }
 }
