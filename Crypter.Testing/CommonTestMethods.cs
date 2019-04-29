b61b73ecd4b69d6c2fe5d9a4d73fb4a5e45152be
Zalkin <Zalkin.viktor@mail.ru>
2019-04-29 22:24:27 +0300

    CT.CMT. Implement TestDifferentShift()

--- Crypter.Testing/CommonTestMethods.cs
+++ Crypter.Testing/CommonTestMethods.cs
@@ -16,6 +16,55 @@ namespace Crypter.Testing
             Assert.AreEqual(result, operation(input, step));
         }
 
+        internal static void TestDifferentShift(CrypterOperation encrypt, CrypterOperation decrypt, string alphabet)
+        {
+            string firstLetter = alphabet[0].ToString();
+            string secondLetter = alphabet[1].ToString();
+            string penultLetter = alphabet[alphabet.Length - 2].ToString();
+            string lastLetter = alphabet[alphabet.Length - 1].ToString();
+
+            string text = firstLetter + lastLetter;
+            int[] steps =
+            {
+                0,
+                1,
+                alphabet.Length - 1,
+                alphabet.Length,
+                alphabet.Length + 1,
+                alphabet.Length * 2,
+                alphabet.Length * 2 + 1
+            };
+
+            string[] expectedEncrypts =
+            {
+                text,
+                secondLetter + firstLetter,
+                lastLetter + penultLetter,
+                text,
+                secondLetter + firstLetter,
+                text,
+                secondLetter + firstLetter
+            };
+
+            string[] expectedDecrypts =
+            {
+                text,
+                lastLetter + penultLetter,
+                secondLetter + firstLetter,
+                text,
+                lastLetter + penultLetter,
+                text,
+                lastLetter + penultLetter
+            };
+
+            for (int i = 0; i < steps.Length; i++)
+            {
+                TestShift(encrypt, text, steps[i], expectedEncrypts[i]);
+                TestShift(decrypt, text, steps[i], expectedDecrypts[i]);
+            }
+
+        }
+
         internal static void TestEncryptFullAlphabet(CrypterOperation operation, string alphabet)
         {
             for (int i = 0; i <= alphabet.Length; i++)
