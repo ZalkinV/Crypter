a56655a671242684b09107a90fae97e0340e13f0
Zalkin <Zalkin.viktor@mail.ru>
2019-04-29 21:40:50 +0300

    CT.ET. Implement TestSimpleShift()

--- Crypter.Testing/EnglishTest.cs
+++ Crypter.Testing/EnglishTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Crypter.Control;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Crypter.Testing
@@ -6,5 +7,16 @@ namespace Crypter.Testing
     [TestClass]
     public class EnglishTest
     {
+        CaesarCrypter crypterEng = new CaesarCrypter(new Alphabet(Languages.English));
+
+        [TestMethod]
+        public void TestSimpleShift()
+        {
+            string text = "bcde";
+            int step = 1;
+
+            CommonTestMethods.TestShift(crypterEng.Encrypt, text, step, "cdef");
+            CommonTestMethods.TestShift(crypterEng.Decrypt, text, step, "abcd");
+        }
     }
 }
